00ccc150db200933dc08f0e9326b23547f8bcb2d
Hiroki Fujii <hfujii@hisystron.com>
2021-09-05 20:57:23 +0900

    記事編集APIを実装

---
+++ src/controllers/secureApi1/update.js
@@ -0,0 +1,19 @@
+var Article = require('../../models').Article;
+var sendError = require('../../utils/sendError');
+
+// update article
+module.exports = (req, res) => {
+    const data = req.body.data;
+    Article.update(
+        {title: data.title, article: data.article, youtube_code: data.youtube_code, released_at: data.released_at},
+        {where: {id: data.id}}
+    ).then((val) =>{
+        res.json({'success': true});
+    }, (err) => {
+        if (process.env.NODE_ENV === 'development') {
+            sendError(err);
+        } else{
+            sendError('update faild.');
+        }
+    })
+}
--- src/routes/secureApi1.js
+++ src/routes/secureApi1.js
@@ -4,6 +4,7 @@ var sendError = require('../utils/sendError');
 
 var createController = require('../controllers/secureApi1/create');
 var deleteController = require('../controllers/secureApi1/delete');
+var updateController = require('../controllers/secureApi1/update');
 
 var router = express.Router();
 
@@ -14,6 +15,7 @@ router.use(middleware.auth);
 /* POST API */
 router.post('/create', createController);
 router.post('/delete', deleteController);
+router.post('/update', updateController);
 
 router.use((err, req, res, next) => {
   sendError(res, err);
